b3784b9ed122f728031f5761cb495cd13a4e57f9
blue-stone <qingshiji96@gmail.com>
2024-02-10 18:07:45 +0800

    bugfix
    in GN, delete "-" before R when calculate Jacobian

--- src/my_icp_base.cpp
+++ src/my_icp_base.cpp
@@ -1,19 +1,18 @@
 #include <my_icp_base.h>
 
-namespace my_icp_base_namespace{
-
-my_icp_base::my_icp_base()
+namespace my_icp_base_namespace
 {
-    std::cerr<<"---my_icp_base constructor---"<<std::endl;
-}
 
-my_icp_base::~my_icp_base()
-{
-    std::cerr<<"---my_icp_base deconstructor---"<<std::endl;
+  my_icp_base::my_icp_base()
+  {
+    std::cerr << "---my_icp_base constructor---" << std::endl;
+  }
 
-}
+  my_icp_base::~my_icp_base()
+  {
+    std::cerr << "---my_icp_base deconstructor---" << std::endl;
+  }
 
-// my_icp_base::align()
+  // my_icp_base::align()
 
-
-}   // my_icp_base_namespace
+} // my_icp_base_namespace
